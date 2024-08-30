3428a7f5107af843db28e90c61fbcf2d879b84d3
roisiano <63460832+roisiano@users.noreply.github.com>
2024-08-30 10:21:40 +0200

    Update process_epg.py

--- .github/workflows/process_epg.py
+++ .github/workflows/process_epg.py
@@ -1,5 +1,6 @@
 import os
-from datetime import datetime
+import re
+from datetime import datetime, timedelta
 import xml.etree.ElementTree as ET
 
 # Directorios y archivos
@@ -7,66 +8,61 @@ output_dir = '/tmp/epg'
 combined_file = f'{output_dir}/EPGcompleto.xml'
 epg_actual_file = f'{output_dir}/epg.xml'
 
-def extract_programmes(file_path):
+def parse_programmes(file_path):
     programmes = []
     tree = ET.parse(file_path)
     root = tree.getroot()
     
     for programme in root.findall('programme'):
-        programmes.append(ET.tostring(programme, encoding='unicode'))
+        start = programme.get('start')
+        stop = programme.get('stop')
+        channel = programme.get('channel')
+        data = ET.tostring(programme, encoding='unicode')
+        programmes.append((start, stop, channel, data))
     
     return programmes
 
-def sort_and_deduplicate_programmes(programmes):
-    programme_dict = {}
-
-    for programme in programmes:
-        tree = ET.fromstring(programme)
-        start_time = tree.get('start')
-        channel = tree.get('channel')
-        key = (channel, start_time)
-        
-        start_time_dt = datetime.strptime(start_time, '%Y%m%d%H%M%S %z')
-        
-        if key not in programme_dict:
-            programme_dict[key] = (programme, start_time_dt)
-        else:
-            existing_programme, existing_time = programme_dict[key]
-            if start_time_dt > existing_time:
-                programme_dict[key] = (programme, start_time_dt)
-
-    unique_programmes = [prog[0] for prog in sorted(programme_dict.values(), key=lambda x: (x[1], x[0]))]
-    
-    return unique_programmes
+def is_overlap(prog1, prog2):
+    start1, stop1 = datetime.strptime(prog1[0], '%Y%m%d%H%M%S %z'), datetime.strptime(prog1[1], '%Y%m%d%H%M%S %z')
+    start2, stop2 = datetime.strptime(prog2[0], '%Y%m%d%H%M%S %z'), datetime.strptime(prog2[1], '%Y%m%d%H%M%S %z')
+    return not (stop1 <= start2 or stop2 <= start1)
 
 def combine_epgs(output_dir, combined_file, epg_actual_file):
+    programmes = []
+    
+    # Añadir el archivo actual completo
+    programmes.extend(parse_programmes(epg_actual_file))
+    
+    # Eliminar archivos antiguos de más de 15 días
+    cutoff_date = datetime.now() - timedelta(days=15)
+    for file in os.listdir(output_dir):
+        if file.endswith('.xml') and file != os.path.basename(epg_actual_file):
+            file_path = os.path.join(output_dir, file)
+            file_mtime = datetime.fromtimestamp(os.path.getmtime(file_path))
+            if file_mtime < cutoff_date:
+                os.remove(file_path)
+                print(f'Removido archivo antiguo: {file_path}')
+    
+    # Listar y procesar archivos de días anteriores
     files = sorted([f for f in os.listdir(output_dir) if f.endswith('.xml') and f != os.path.basename(epg_actual_file)],
                    key=lambda x: os.path.getmtime(os.path.join(output_dir, x)),
                    reverse=True)
 
-    all_programmes = []
-
-    # Añadir el archivo actual completo
-    with open(epg_actual_file, 'r') as infile:
-        all_programmes.append(infile.read())
-    
-    # Añadir programas de archivos previos, ordenando por fecha de archivo (más reciente primero)
-    programmes = []
     for epg_file in files:
         file_path = os.path.join(output_dir, epg_file)
-        programmes.extend(extract_programmes(file_path))
-    
-    programmes.extend(extract_programmes(epg_actual_file))
-    
-    # Ordenar y deduplicar todos los programas
-    programmes = sort_and_deduplicate_programmes(programmes)
-    
+        previous_programmes = parse_programmes(file_path)
+        
+        # Filtrar programas no solapados
+        for prog in previous_programmes:
+            if all(not is_overlap(prog, p) for p in programmes):
+                programmes.append(prog)
+
     # Escribir el archivo combinado
     with open(combined_file, 'w') as outfile:
         outfile.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         outfile.write('<tv>\n')
-        outfile.write(''.join(programmes))
+        for prog in programmes:
+            outfile.write(prog[3])
         outfile.write('</tv>\n')
 
 combine_epgs(output_dir, combined_file, epg_actual_file)
-
