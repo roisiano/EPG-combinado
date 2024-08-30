247e48bc5785608686a289897e633b4b7c266671
roisiano <63460832+roisiano@users.noreply.github.com>
2024-08-30 11:15:42 +0200

    Update process_epg_initial.py

--- .github/workflows/process_epg_initial.py
+++ .github/workflows/process_epg_initial.py
@@ -2,28 +2,34 @@ import os
 import requests
 import gzip
 import shutil
+from datetime import datetime
 
-def download_and_extract_epg(url, output_dir, output_file):
-    """Download and extract EPG file from URL."""
-    response = requests.get(url, stream=True)
-    gz_file_path = os.path.join(output_dir, 'epg.xml.gz')
-    
-    # Save the gzipped file
-    with open(gz_file_path, 'wb') as f:
-        f.write(response.content)
-    
-    # Decompress the gzipped file
-    with gzip.open(gz_file_path, 'rb') as f_in:
-        with open(output_file, 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
+# Configuración
+url = "https://github.com/davidmuma/EPG_dobleM/raw/master/guiatv_color.xml.gz"
+output_file_path = '/tmp/epg/epg.xml.gz'
+today_date = datetime.now().strftime('%Y%m%d')
 
-def main():
-    epg_url = 'https://github.com/davidmuma/EPG_dobleM/raw/master/guiatv_color.xml.gz'
-    output_dir = '/tmp/epg'
-    os.makedirs(output_dir, exist_ok=True)
-    output_file = os.path.join(output_dir, 'EPGactual.xml')
+# Crear el directorio de trabajo
+os.makedirs('/tmp/epg', exist_ok=True)
 
-    download_and_extract_epg(epg_url, output_dir, output_file)
+# Descargar el archivo comprimido
+response = requests.get(url)
+with open(output_file_path, 'wb') as f:
+    f.write(response.content)
 
-if __name__ == "__main__":
-    main()
+# Descomprimir el archivo
+with gzip.open(output_file_path, 'rb') as f_in:
+    with open('/tmp/epg/epg.xml', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+# Leer el archivo XML descomprimido
+with open('/tmp/epg/epg.xml', 'r') as file:
+    epg_content = file.read()
+
+# Crear la carpeta epg_files si no existe
+os.makedirs('epg_files', exist_ok=True)
+
+# Guardar el archivo actual EPGactual.xml
+with open(f'epg_files/{today_date}.xml', 'w') as file:
+    file.write(epg_content)
+
