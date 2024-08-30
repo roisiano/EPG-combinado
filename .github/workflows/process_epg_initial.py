eef0cca451d5819dab5722738c3adeb01eb81366
roisiano <63460832+roisiano@users.noreply.github.com>
2024-08-30 11:52:09 +0200

    Update process_epg_initial.py

--- .github/workflows/process_epg_initial.py
+++ .github/workflows/process_epg_initial.py
@@ -1,45 +1,17 @@
-import os
-import requests
-import gzip
 import shutil
-import datetime
 
-def download_and_extract_epg():
-    url = "https://github.com/davidmuma/EPG_dobleM/raw/master/guiatv_color.xml.gz"
-    response = requests.get(url)
-    with open('/tmp/epg/epg.xml.gz', 'wb') as f:
-        f.write(response.content)
-    with gzip.open('/tmp/epg/epg.xml.gz', 'rb') as f_in:
-        with open('/tmp/epg/epg.xml', 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
-    print("EPG file downloaded and extracted.")
-
-def generate_daily_files():
-    with open('/tmp/epg/epg.xml', 'r') as file:
-        content = file.read()
-
-    current_date = datetime.datetime.now().strftime("%d.%m.%Y")
-    output_file = f'/tmp/epg/EPGactual.xml'
-    with open(output_file, 'w') as file:
-        file.write(content)
-    print(f"Generated file: {output_file}")
-
-    days_to_keep = 5
-    for i in range(days_to_keep, 0, -1):
-        date_str = (datetime.datetime.now() - datetime.timedelta(days=i)).strftime("%d.%m.%Y")
-        daily_file_path = f'/tmp/epg/{date_str}.xml'
-        if os.path.exists(daily_file_path):
-            continue
-        with open(daily_file_path, 'w') as file:
-            file.write(content)
-        print(f"Generated file: {daily_file_path}")
+def process_initial_epg():
+    source_file = '/tmp/epg/epg.xml'
+    dest_file = '/tmp/epg/EPGactual.xml'
+    shutil.copy(source_file, dest_file)
+    print(f"EPGactual.xml generated: {dest_file}")
 
 def main():
-    download_and_extract_epg()
-    generate_daily_files()
+    process_initial_epg()
 
 if __name__ == "__main__":
     main()
+
 
 
 
